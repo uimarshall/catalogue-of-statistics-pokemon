5698bb3ddca1733b0fa877ea56bcc25adc965aea
uimarshall <uimarshall@gmail.com>
2021-03-05 01:15:46 +0100

    Refactor error alerts

--- src/utils/Alert.js
+++ src/utils/Alert.js
@@ -1,13 +1,19 @@
 import { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
+import { useHistory } from 'react-router-dom';
 
 function AlertDismissible() {
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
   const [show, setShow] = useState(true);
 
   if (show) {
     return (
-      <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+      <Alert variant="danger" onClose={() => setShow(false)} dismissible className="text-center">
         <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
         <p>
           Check what you typed and try again. .
@@ -15,7 +21,7 @@ function AlertDismissible() {
       </Alert>
     );
   }
-  return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+  return <Button onClick={() => handleGoBack()} className="mx-auto mt-4">Go Back</Button>;
 }
 
 export default AlertDismissible;
